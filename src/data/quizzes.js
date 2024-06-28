4e74a3f7ad2cf29c03918f57bab5a4c56a4453f5
hamzakhalem <hamzakhalem97@gmail.com>
2024-06-28 23:14:08 +0100

    add db

--- src/data/quizzes.js
+++ src/data/quizzes.js
@@ -1,7 +1,7 @@
 // src/data/quizzes.js
 export const quizzes = [
     {
-      category: 'Science',
+      category: 'Sience',
       questions: [
         {
           question: 'What is the chemical symbol for water?',
@@ -19,42 +19,78 @@ export const quizzes = [
       ],
     },
     {
-      category: 'Tiktok',
+      category: 'رياضة',
       questions: [
         {
-          question: 'وش نسمي Enzo',
-          options: ['الشاوي', 'العنزي', 'العدو', 'لا شيئ مما سبق'],
-          answer: 'العنزي',
+          question: 'من اللاعب الذي سجل ضد مصر في ام درمان',
+          options: ['عبد القادر غزال', 'عباس بن فرناس', 'مصطفى كلاه النمر', 'عنتر يحيى'],
+          answer: 'عنتر يحيى',
           number: 1
         },
         {
-          question: 'امينة علاش غاملة',
-          options: ['تكرهني', 'تغير مني', 'متحبش الشجر', 'انا الاحسن'],
-          answer:'تغير مني', 
+          question: 'من فاز بكاس امم افريقيا سنة 2004',
+          options: ['البرتغال', 'شباب عين بنيان', 'تونس', 'الكاميرون'],
+          answer: 'تونس', 
           number: 2
         },
         {
-          question: 'kitten is ',
-          options: ['Nurse', 'Tebiba', 'Tebi3 left', 'ربة بيت'],
-          answer: 'Nurse', 
+          question: 'من هو النادي الذي يلقب بالنعامة',
+          options: ['MCO', 'برشلونة', 'CRB', 'اهلي برج بوعنينيج'],
+          answer: 'CRB', 
+          number: 3
+        },
+        {
+          question: 'من هو اللاعب الذي كان هداف البطولة الجزائرية 2013',
+          options: ['ياشير', 'الدون بلعويدات', 'جاليط', 'يوسف بلايلي'],
+          answer: 'جاليط', 
+          number: 4
+        },
+        {
+          question: 'اخر لقب حققه كبير لندن توتنهام',
+          options: ['2004', '2009', '2007',  '2008'],
+          answer: 'جاليط', 
+          number: 5
+        },
+        // More questions...
+      ],
+    },
+    {
+      category: 'سياسة',
+      questions: [
+        {
+          question: 'من القائل  "جوع كلبك يتبع" ؟',
+          options: ['حميد عاشوري', 'اويحيى الموسطاش', 'حميد لالاوي', 'لا شيئ مما سبق'],
+          answer: 'اويحيى الموسطاش',
+          number: 1
+        },
+        {
+          question: 'من قال سامية زيدي للبوسط ؟',
+          options: ['سلال', 'حميد عاشوري مجددا', 'حسان النية', 'موح ميلانو تاع سوسطارة'],
+          answer:'سلال', 
           number: 2
         },
         {
-          question: 'Omaima best hobby is ?',
-          options: ['Dance', 'Kill', 'Paint', 'Singing'],
-          answer: 'Paint', 
+          question: 'من هو الرئيس الجزائري الاكثر حكما',
+          options: ['بومدين', 'بوتفليفة', 'الشاذلي بن جديد',  'زروال اليمين'],
+          answer: 'بوتفليفة', 
+          number: 3
+        },
+        {
+          question: 'ما هو نظام الحكم في بريطانيا',
+          options: ['شاوي', 'ملكي', 'برلماني', 'سابق ولاحق'],
+          answer: 'ملكي', 
+          number: 4
+        },
+        {
+          question: 'في اي عهدة من بوتفليقة تم تعديل الدستور',
+          options: ['الاولى', 'الثانية', 'الخاممسة', 'الرابعة'],
+          answer: 'الثانية', 
           number: 2
         },
         {
-          question: 'Foufou is ?',
-          options: ['Kbaylia hachkom', 'Stafiya astghfirallh', 'S3oudia', 'habacha'],
-          answer: 'kbaylia hachkom', 
-          number: 2
-        },
-        {
-          question: 'GIGI best صفة is ?',
-          options: ['Mo2mina', 'chitana', 'tbi3 left hiya tani', 'teheb le9touta'],
-          answer: 'Mo2mina', 
+          question: 'من هو رشيد نقاز ؟',
+          options: ['وليد عمة رشيد نكاز', 'وليد محمد نقاز', 'طباخ', 'ميكانسيان'],
+          answer: 'ميكانيسان', 
           number: 2
         },
         // More questions...
