d82dc9e1966d6de102c52a943fe69731e24d4568
hamzakhalem <hamzakhalem97@gmail.com>
2024-07-15 21:15:43 +0100

    fix: errors

--- src/data/quizzes.js
+++ src/data/quizzes.js
@@ -1,24 +1,6 @@
-// src/data/quizzes.js
+// QUIZ DATA
 
 export const quizzes = [
-    // {
-    //   category: 'Sience',
-    //   questions: [
-    //     {
-    //       question: 'What is the chemical symbol for water?',
-    //       options: ['H2O', 'O2', 'H2', 'CO2'],
-    //       answer: 'H2O',
-    //       number: 1
-    //     },
-    //     {
-    //       question: 'what is the color of sky',
-    //       options: ['Red', 'Blue', 'Green', 'Grey'],
-    //       answer: 'Blue', 
-    //       number: 2
-    //     },
-    //     // More questions...
-    //   ],
-    // },
     {
       category: 'رياضة',
       questions: [
